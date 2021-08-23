8a0bbb911211b9971a17c1581717e2cf64d3275f
Edward Higgins <ed.higgins@york.ac.uk>
2021-08-23 18:03:25 +0100

    Adds relevant files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='simpletest',
+      version='1.0',
+      description='Some random test thing',
+      author='Ed Higgins',
+      author_email='ed.higgins@york.ac.uk',
+      url='https://company.website',
+      packages=['simpletest'],
+     )
